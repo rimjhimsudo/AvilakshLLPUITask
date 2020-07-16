3870880408435d8a53848f5c02a37b1c57596181
rimjhimsudo <rimjhimvaish@gmail.com>
2020-07-16 14:20:39 +0530

    fnal commit

--- app/src/main/java/com/example/rimjhimintern/MainActivity.java
+++ app/src/main/java/com/example/rimjhimintern/MainActivity.java
@@ -6,12 +6,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.badge.BadgeDrawable;
@@ -21,8 +25,9 @@ import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class MainActivity extends AppCompatActivity {
     MaterialSearchView searchView;
-    ImageView imageView;
+    ImageView imageView , arrow_loc;
     BottomNavigationView bottomNavigationItemView;
+    Spinner spinnerloc, spinner_servces;
     RecyclerView recyclervw;
     BadgeDrawable badgeDrawable,badgeDrawable1;
     private Activity context;
@@ -30,10 +35,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Window window = getWindow();
         bottomNavigationItemView=findViewById(R.id.bottom_navigation);
+        arrow_loc=findViewById(R.id.arrow_loc);
+        spinnerloc=findViewById(R.id.dropdown_location);
+        spinner_servces=findViewById(R.id.sp_search);
+        ArrayAdapter adapterloc = ArrayAdapter.createFromResource(this, R.array.locatn_arrays, R.layout.spinner_loc);
+        adapterloc.setDropDownViewResource(R.layout.spinner_dropdown_loc);
+        spinnerloc.setAdapter(adapterloc);
+        ArrayAdapter<String> adapterservces = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.servce_arrays));
+        adapterservces.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_servces.setAdapter(adapterservces);
+        arrow_loc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(spinnerloc.getSelectedItem() == null) {
+                    spinnerloc.performClick();
+                }
+            }
+        });
+        //spinnerloc.getBackground().setColorFilter(getResources().getColor(R.color.colorWhite), PorterDuff.Mode.SRC_ATOP);
+
+
+
         // clear FLAG_TRANSLUCENT_STATUS flag:
-        /*window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        /* Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
         // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
